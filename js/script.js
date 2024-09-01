e636c538674ecd9faefae4688bd7b33d21badc68
AsitChandraRoy <asitchandraroy@gmail.com>
2024-09-01 16:29:07 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -7,46 +7,40 @@ let editTodo = null;
 
 // add to do Function
 const addTodo = () => {
-  if(inputBox instanceof HTMLInputElement){
+  const inputText = inputBox?.value.trim();
+  if (inputText.length <= 0) {
+    alert("input a task");
 
-    const inputText = inputBox?.value.trim();
-    if (inputText.length <= 0) {
-      alert("input a task");
-
-      return 0;
-    }
-    if(addBtn instanceof HTMLInputElement){
-
-      if (addBtn?.value === "Edit") {
-        editTodo.target.previousElementSibling.innerHTML = inputText;
-        addBtn.value = "Add";
-        if(inputBox){
-
-          inputBox.value = "";
-        }
-      } else {
-        // creating p tag
-        const li = document.createElement("li");
-        const p = document.createElement("p");
-        p.setAttribute("class", "flex-grow text-start");
-        li.setAttribute(
-          "class",
-          "border w-96 mx-auto flex justify-between items-center py-2 px-2 rounded-md bg-[#ffffff]"
-        );
-        p.innerHTML = inputText;
-        li.appendChild(p);
-
-        // creating edit button
-        const editBtn = document.createElement("button");
-        editBtn.innerText = "Edit";
-        li.appendChild(editBtn);
-        editBtn.setAttribute(
-          "class",
-          "bg-green-500 hover:bg-green-600 rounded-md px-5 py-2 me-2"
-        );
-    }
+    return 0;
   }
 
+  if (addBtn?.value === "Edit") {
+    editTodo.target.previousElementSibling.innerHTML = inputText;
+    addBtn.value = "Add";
+    if(inputBox){
+
+      inputBox.value = "";
+    }
+  } else {
+    // creating p tag
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.setAttribute("class", "flex-grow text-start");
+    li.setAttribute(
+      "class",
+      "border w-96 mx-auto flex justify-between items-center py-2 px-2 rounded-md bg-[#ffffff]"
+    );
+    p.innerHTML = inputText;
+    li.appendChild(p);
+
+    // creating edit button
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    li.appendChild(editBtn);
+    editBtn.setAttribute(
+      "class",
+      "bg-green-500 hover:bg-green-600 rounded-md px-5 py-2 me-2"
+    );
 
     // creating delete button
     const deleteBtn = document.createElement("button");
@@ -72,12 +66,12 @@ const updateTodo = (e) => {
   }
 
   if (e.target.innerHTML === "Edit") {
-    if(inputBox instanceof HTMLInputElement){
+    if(inputBox){
 
       inputBox.value = e.target.previousElementSibling.innerHTML;
     }
     inputBox?.focus();
-    if(addBtn instanceof HTMLInputElement){
+    if(addBtn){
 
       addBtn.value = "Edit";
     }
